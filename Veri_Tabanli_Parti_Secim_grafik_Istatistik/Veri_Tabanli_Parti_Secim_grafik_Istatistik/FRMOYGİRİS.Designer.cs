85251a29535275f172a44a6b18e2480e0013b561
Emir <emirkan.ulker9@gmail.com>
2023-10-06 00:28:51 +0300

    Veri_Tabani_Grafik_Sistemi

---
+++ Veri_Tabanli_Parti_Secim_grafik_Istatistik/Veri_Tabanli_Parti_Secim_grafik_Istatistik/FRMOYGİRİS.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Veri_Tabanli_Parti_Secim_grafik_Istatistik
+{
+    partial class FRMOYGİRİS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txilcead = new System.Windows.Forms.TextBox();
+            this.txaparti = new System.Windows.Forms.TextBox();
+            this.txbparti = new System.Windows.Forms.TextBox();
+            this.txcparti = new System.Windows.Forms.TextBox();
+            this.txdparti = new System.Windows.Forms.TextBox();
+            this.txeparti = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btngirisyap = new System.Windows.Forms.Button();
+            this.btnistatis = new System.Windows.Forms.Button();
+            this.btngrafik = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txilcead
+            // 
+            this.txilcead.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txilcead.Location = new System.Drawing.Point(113, 33);
+            this.txilcead.Name = "txilcead";
+            this.txilcead.Size = new System.Drawing.Size(305, 22);
+            this.txilcead.TabIndex = 0;
+            // 
+            // txaparti
+            // 
+            this.txaparti.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txaparti.Location = new System.Drawing.Point(113, 84);
+            this.txaparti.Name = "txaparti";
+            this.txaparti.Size = new System.Drawing.Size(305, 22);
+            this.txaparti.TabIndex = 1;
+            // 
+            // txbparti
+            // 
+            this.txbparti.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txbparti.Location = new System.Drawing.Point(113, 136);
+            this.txbparti.Name = "txbparti";
+            this.txbparti.Size = new System.Drawing.Size(305, 22);
+            this.txbparti.TabIndex = 2;
+            // 
+            // txcparti
+            // 
+            this.txcparti.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txcparti.Location = new System.Drawing.Point(113, 189);
+            this.txcparti.Name = "txcparti";
+            this.txcparti.Size = new System.Drawing.Size(305, 22);
+            this.txcparti.TabIndex = 3;
+            // 
+            // txdparti
+            // 
+            this.txdparti.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txdparti.Location = new System.Drawing.Point(113, 238);
+            this.txdparti.Name = "txdparti";
+            this.txdparti.Size = new System.Drawing.Size(305, 22);
+            this.txdparti.TabIndex = 4;
+            // 
+            // txeparti
+            // 
+            this.txeparti.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txeparti.Location = new System.Drawing.Point(113, 291);
+            this.txeparti.Name = "txeparti";
+            this.txeparti.Size = new System.Drawing.Size(305, 22);
+            this.txeparti.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(45, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "İLÇE AD:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(45, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "A PARTİ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(45, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B PARTİ:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(45, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "C PARTİ:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(45, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "D PARTİ:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(45, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "E PARTİ:";
+            // 
+            // btngirisyap
+            // 
+            this.btngirisyap.BackColor = System.Drawing.Color.OldLace;
+            this.btngirisyap.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btngirisyap.Location = new System.Drawing.Point(172, 327);
+            this.btngirisyap.Name = "btngirisyap";
+            this.btngirisyap.Size = new System.Drawing.Size(123, 35);
+            this.btngirisyap.TabIndex = 12;
+            this.btngirisyap.Text = "OY GİRİŞİ YAP";
+            this.btngirisyap.UseVisualStyleBackColor = false;
+            this.btngirisyap.Click += new System.EventHandler(this.btngirisyap_Click);
+            // 
+            // btnistatis
+            // 
+            this.btnistatis.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnistatis.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnistatis.Location = new System.Drawing.Point(295, 370);
+            this.btnistatis.Name = "btnistatis";
+            this.btnistatis.Size = new System.Drawing.Size(123, 35);
+            this.btnistatis.TabIndex = 13;
+            this.btnistatis.Text = "Çıkış Yap";
+            this.btnistatis.UseVisualStyleBackColor = false;
+            this.btnistatis.Click += new System.EventHandler(this.btnistatis_Click);
+            // 
+            // btngrafik
+            // 
+            this.btngrafik.BackColor = System.Drawing.Color.DarkOrange;
+            this.btngrafik.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btngrafik.Location = new System.Drawing.Point(48, 368);
+            this.btngrafik.Name = "btngrafik";
+            this.btngrafik.Size = new System.Drawing.Size(123, 35);
+            this.btngrafik.TabIndex = 14;
+            this.btngrafik.Text = "GRAFİKLER";
+            this.btngrafik.UseVisualStyleBackColor = false;
+            this.btngrafik.Click += new System.EventHandler(this.btngrafik_Click);
+            // 
+            // FRMOYGİRİS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(450, 417);
+            this.Controls.Add(this.btngrafik);
+            this.Controls.Add(this.btnistatis);
+            this.Controls.Add(this.btngirisyap);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txeparti);
+            this.Controls.Add(this.txdparti);
+            this.Controls.Add(this.txcparti);
+            this.Controls.Add(this.txbparti);
+            this.Controls.Add(this.txaparti);
+            this.Controls.Add(this.txilcead);
+            this.Name = "FRMOYGİRİS";
+            this.Text = "VTGS";
+            this.Load += new System.EventHandler(this.FRMOYGİRİS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txilcead;
+        private System.Windows.Forms.TextBox txaparti;
+        private System.Windows.Forms.TextBox txbparti;
+        private System.Windows.Forms.TextBox txcparti;
+        private System.Windows.Forms.TextBox txdparti;
+        private System.Windows.Forms.TextBox txeparti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btngirisyap;
+        private System.Windows.Forms.Button btnistatis;
+        private System.Windows.Forms.Button btngrafik;
+    }
+}
+
---
+++ Veri_Tabanli_Parti_Secim_grafik_Istatistik/Veri_Tabanli_Parti_Secim_grafik_Istatistik/FRMOYGİRİS.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Veri_Tabanli_Parti_Secim_grafik_Istatistik
+{
+    public partial class FRMOYGİRİS : Form
+    {
+        public FRMOYGİRİS()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-BJO2DGU\SQLEXPRESS;Initial Catalog=DBSECIMPROJE;Integrated Security=True");
+       
+        private void btngirisyap_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into TBLILCE (ILCEAD,APARTI,BPARTI,CPARTI,DPARTI,EPARTI) values (@p1,@p2,@p3,@p4,@p5,@p6)", baglanti);
+            komut.Parameters.Add("@p1", SqlDbType.VarChar).Value = txilcead.Text;
+            komut.Parameters.Add("@p2", SqlDbType.TinyInt).Value = Convert.ToInt16(txaparti.Text);
+            komut.Parameters.Add("@p3", SqlDbType.TinyInt).Value = Convert.ToInt16(txbparti.Text);
+            komut.Parameters.Add("@p4", SqlDbType.TinyInt).Value = Convert.ToInt16(txcparti.Text);
+            komut.Parameters.Add("@p5", SqlDbType.TinyInt).Value = Convert.ToInt16(txdparti.Text);
+            komut.Parameters.Add("@p6", SqlDbType.TinyInt).Value = Convert.ToInt16(txeparti.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Oy Girişi Gerçekleşti");
+
+        }
+
+        private void FRMOYGİRİS_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btngrafik_Click(object sender, EventArgs e)
+        {
+            FrmGrafikler frg = new FrmGrafikler();
+            frg.Show();
+            this.Hide();
+        }
+
+        private void btnistatis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
